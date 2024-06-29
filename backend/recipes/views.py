740b35c15d8ef4ea1ef2eaa85dfe143ea66e8bc4
Jesse-L2 <jesse.little@utexas.edu>
2024-06-29 10:16:53 -0500

    Start views.py for recipes

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Author, Recipe
 
 # Create your views here.
+def recipesView(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'recipes.html', {'recipes': recipes})
