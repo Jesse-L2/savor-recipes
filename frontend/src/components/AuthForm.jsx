f6990787f84a6cc8fcc292d907ee32d3ccf1d785
Jesse-L2 <jesse.little@utexas.edu>
2024-07-05 12:23:45 -0500

    Start authform

---
+++ frontend/src/components/AuthForm.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import api from "../../api";
+
+import React from "react";
+
+const AuthForm = () => {
+  return <div>AuthForm</div>;
+};
+
+export default AuthForm;
