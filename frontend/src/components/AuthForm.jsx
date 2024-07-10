69c2aa878c42843c8fc8c36e5db0b6341c1a60bb
Jesse-L2 <jesse.little@utexas.edu>
2024-07-10 13:42:57 -0500

    Fix api import in AuthForm and get in urls.py

--- frontend/src/components/AuthForm.jsx
+++ frontend/src/components/AuthForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
-import api from "../../api";
+import api from "../api";
 
 import React from "react";
 
