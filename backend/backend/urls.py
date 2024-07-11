63f24995d8b1062ce358d02af5f22e60ae11fdd5
Jesse-L2 <jesse.little@utexas.edu>
2024-07-11 11:21:41 -0500

    Update backend urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,11 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from recipes.views import CreateUserView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('recipes.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('api/user/register/', CreateUserView.as_view(), name='register')
 ]
