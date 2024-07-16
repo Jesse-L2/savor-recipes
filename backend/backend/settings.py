88d9659e21d6f5fe4f162b77c5fac1648f8e49cf
Jesse-L2 <jesse.little@utexas.edu>
2024-07-16 16:10:04 -0500

    Update settings.py for JWT auth

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,6 +32,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+}
 
 # Application definition
 
@@ -84,8 +88,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     "default": {
-        # "ENGINE": "django.db.backends.sqlite3",
-        # "NAME": BASE_DIR / "db.sqlite3",
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
