e96afc95da3daa38c9fe77c53becffbe1b88b49e
Jesse-L2 <jesse.little@utexas.edu>
2025-04-14 10:12:27 -0500

    Finish basic backend w/ jwt tokens

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,60 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from recipes.models import Recipe
+from users.models import User
+from .serializers import RecipeSerializer, UserSerializer
 
-# Create your views here.
+# TODO: Check Class inheritances
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+    permission_classes = [AllowAny]
+    
+    def get_queryset(self):
+        queryset = Recipe.objects.all()
+        user_id = self.request.query_params.get('user', None)
+        if user_id:
+            queryset = queryset.filter(user__id=user_id)
+
+        return queryset
+    
+    def perform_create(self, serializer):
+        # override create w/ custom create
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+class RecipeListCreate(viewsets.ModelViewSet):
+    serializer_class = RecipeSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Recipe.objects.all()
+    
+    def perform_create(self, serializer):
+        # override create w/ custom create
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+class RecipeDelete(viewsets.ModelViewSet):
+    serializer_class = RecipeSerializer
+    permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        user = self.request.user
+        return Recipe.objects
+    
+class CreateUserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+    
+    def get_permissions(self):
+        if self.action == 'create':
+            return [AllowAny()]
+        return [IsAuthenticated()]
